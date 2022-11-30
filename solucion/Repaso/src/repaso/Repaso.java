371d5ec34edec1a8ca9e96e79749541752e00a03
Mariangel Rojas <115736474+mariangel17@users.noreply.github.com>
2022-11-30 13:32:19 -0500

    Taller repaso

--- solucion/Repaso/src/repaso/Repaso.java
+++ solucion/Repaso/src/repaso/Repaso.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package repaso;
 
@@ -11,69 +10,69 @@ import java.util.Scanner;
 
 /**
  *
- * @author SALA I
+ * @author Usuario PC
  */
 public class Repaso {
-
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner entrada = new Scanner (System.in);
-        entrada.useLocale(Locale.US);
-        String nombre="";
-        int numero_empleados=0;
-        int cedula= 0;
-        int horas_matutinas= 0;
-        int hora_matutina_adicional;
-        int hora_nocturna_adicional;
-        int horas_nocturnas= 0;
-        String ciudad;
-        double sueldo_final;
-        int contador;
-        double horas_total=0;
-        String opcion = null;
-        double total_horas_matutinas;
-        double total_horas_nocturnas;
-        String informe="Rol del trabajador";
-        double valor_seguro_social= 0;
-        int valor_hora_total=0;
-        int horas_matutinas_total = 0;
-        int horas_nocturnas_total = 0;
-   
+     Scanner entrada = new Scanner(System.in);
+     entrada.useLocale(Locale.US);
+    int numEmpleados = 0;
+    int horasMatutinas = 0;
+    int horasNocturnas = 0;
+    int horasTotales = 0;
+    double valorHoraMatutina = 10;
+    double valorHoraNocturna = 15;
+    double valorHoraNocturnaAdicional = 0;
+    double valorHoraNocturnaAdicionalTotal = 0;
+    double valorHoraNocturnaTotal = 0;
+    double valorHoraMatutinaTotal = 0;
+    double valorHoraTotal = 0;
+    double valorSeguroSocial = 0;
+    double valorTotal = 0;
+    String nombre = "";
+    String cedula = "";
+    String ciudad = "";
+    String continuar = "";
+    boolean bandera = true;
+    int contador = 0;
+    String reporte="Nomina de Trabajadores\n";
+    while(bandera){
+     
         System.out.println("Ingrese el nombre del empleado");
-        nombre=entrada.nextLine();
+        nombre = entrada.nextLine();
         System.out.println("Ingrese la cédula del empleado");
-        cedula=entrada.nextInt();
-        System.out.println("Ingrese las horas matutinas");
-        horas_matutinas = entrada.nextInt();
-        System.out.println("Ingrese las horas nocturnas");
-        horas_nocturnas = entrada.nextInt();
+        cedula = entrada.nextLine();
         System.out.println("Ingrese la ciudad de la empresa");
         ciudad = entrada.nextLine();
-        horas_total= horas_matutinas + horas_nocturnas;
-        total_horas_matutinas= horas_matutinas * horas_matutinas_total;
-        total_horas_nocturnas= horas_nocturnas * horas_nocturnas_total;
-        if (horas_nocturnas <= 10 ){
-          total_horas_nocturnas= total_horas_nocturnas*1.1;
-           }
-        valor_seguro_social= valor_hora_total*0.18;
-        valor_hora_total = horas_matutinas +horas_nocturnas;
-        horas_total= valor_hora_total + valor_seguro_social;
-    
-        informe = String.format("%s\nRol del trabajador%s y cédula%s\n "
-                + "+horas_matutinas%d\n"+ "horas_nocturnas%d\n",informe,cedula,horas_matutinas,horas_nocturnas);
-        
-        
-
-
+        System.out.println("Ingrese las horas matutinas trabajadas");
+        horasMatutinas = entrada.nextInt();
+        System.out.println("Ingrese las horas nocturnas trabajadas");
+        horasNocturnas = entrada.nextInt();
+        horasTotales = horasMatutinas + horasNocturnas;
+        valorHoraMatutinaTotal = horasMatutinas * valorHoraMatutina;
+        valorHoraNocturnaTotal = horasNocturnas * valorHoraNocturna;
+        if (horasNocturnas >= 10) {
+            valorHoraNocturnaAdicional = valorHoraNocturna * 0.1;
+            valorHoraNocturnaAdicionalTotal = valorHoraNocturnaAdicional * horasNocturnas;
+            valorHoraNocturnaTotal = valorHoraNocturnaTotal + valorHoraNocturnaAdicionalTotal;
         }
-        
-        }
-        
-        
+        valorHoraTotal = valorHoraMatutinaTotal + valorHoraNocturnaTotal;
+        valorSeguroSocial = valorHoraTotal * 0.18;
+        valorTotal = valorHoraTotal - valorSeguroSocial;
+        reporte = String.format("%s\nRol del trabajador de nombre %s y cédula %s.\n"
+                + "Horas matutina trabajadas: %d.\n"
+                + "Horas nocturnas trabajadas: %d.\n"
+                + "Subtotal a cancelar $%.2f\n"
+                + "Seguro Social $%.2f\n"
+                + "Total a cancelar $%.2f\n", reporte, nombre, cedula, horasMatutinas, horasNocturnas, valorHoraTotal, valorSeguroSocial, valorTotal);
+        System.out.println("Desea ingresar otro empleado? Ingrese Si");
+        continuar = entrada.next();
+        if (continuar.equals("Si")) {
+                bandera = false;
+                }
+        System.out.println(reporte);
+        System.out.println("Numero de empleados: " + contador);
    
-
-      
+    }
+    }
+    }    
